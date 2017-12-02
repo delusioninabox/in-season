97f805d4c66f8d830e975d947f479e67aa62d84f
Laura Kajpust <lkajpust@gmail.com>
2017-12-01 19:54:01 -0500

    More Veggies, Alphabatize, UI improvements

--- app/services/produce.js
+++ app/services/produce.js
@@ -11,8 +11,32 @@ const produceItems = [
   Produce.create({type: 'vegetable', name: 'Broccoli', src: '../assets/broccoli.jpg', available: [1, 2, 3, 4, 5, 10, 11, 12]}),
   Produce.create({type: 'vegetable', name: 'Brussel Sprouts', src: '../assets/brusselsprouts.jpg', available: [1, 2, 3, 4, 5, 9, 10, 11]}),
   Produce.create({type: 'vegetable', name: 'Butternut Squash', src: '../assets/butternutsquash.jpg', available: [8, 9, 10]}),
+  Produce.create({type: 'vegetable', name: 'Carrots', src: '../assets/carrots.jpg', available: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]}),
+  Produce.create({type: 'vegetable', name: 'Cauliflower', src: '../assets/cauliflower.jpg', available: [1, 2, 3, 4, 9, 10, 11, 12]}),
+  Produce.create({type: 'vegetable', name: 'Celery', src: '../assets/celery.jpg', available: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]}),
   Produce.create({type: 'vegetable', name: 'Corn', src: '../assets/corn.jpg', available: [6, 7, 8, 9, 10, 11]}),
+  Produce.create({type: 'vegetable', name: 'Cucumbers', src: '../assets/cucumbers.jpg', available: [6, 7, 8, 9, 10, 11]}),
+  Produce.create({type: 'vegetable', name: 'Eggplant', src: '../assets/eggplant.jpg', available: [6, 7, 8, 9, 10]}),
+  Produce.create({type: 'vegetable', name: 'Green Beans', src: '../assets/greenbeans.jpg', available: [7, 8, 9]}),
+  Produce.create({type: 'vegetable', name: 'Kale', src: '../assets/kale.jpg', available: [1, 2]}),
+  Produce.create({type: 'vegetable', name: 'Leeks', src: '../assets/leeks.jpg', available: [1, 2, 3, 4, 10]}),
+  Produce.create({type: 'vegetable', name: 'Lettuce', src: '../assets/lettuce.jpg', available: [3, 4, 5, 6, 7, 8, 9, 10, 12]}),
+  Produce.create({type: 'vegetable', name: 'Mushrooms', src: '../assets/mushrooms.jpg', available: [1, 2, 3, 4, 8, 9, 10, 11, 12]}),
+  Produce.create({type: 'vegetable', name: 'Okra', src: '../assets/okra.jpg', available: [5, 6, 7, 8, 9, 10, 11]}),
+  Produce.create({type: 'vegetable', name: 'Parsnips', src: '../assets/parsnips.jpg', available: [1, 2, 3, 4, 5, 10, 11, 12]}),
+  Produce.create({type: 'vegetable', name: 'Pumpkins', src: '../assets/pumpkins.jpg', available: [9, 10, 11]}),
+  Produce.create({type: 'vegetable', name: 'Radishes', src: '../assets/radishes.jpg', available: [3, 4, 5]}),
+  Produce.create({type: 'vegetable', name: 'Rhubarb', src: '../assets/rhubarb.jpg', available: [4, 5]}),
+  Produce.create({type: 'vegetable', name: 'Rutabagas', src: '../assets/rutabagas.jpg', available: [1, 2, 3, 4, 10, 11, 12]}),
+  Produce.create({type: 'vegetable', name: 'Spinach', src: '../assets/spinach.jpg', available: [9, 10, 11]}),
+  Produce.create({type: 'vegetable', name: 'Spring Peas', src: '../assets/springpeas.jpg', available: [4,5]}),
+  Produce.create({type: 'vegetable', name: 'Summer Squash', src: '../assets/summersquash.jpg', available: [6, 7, 8, 9, 10]}),
+  Produce.create({type: 'vegetable', name: 'Sweet Potatoes', src: '../assets/sweetpotatoes.jpg', available: [9, 10, 11, 12]}),
+  Produce.create({type: 'vegetable', name: 'Swiss Chard', src: '../assets/swisschard.jpg', available: [1, 2, 5, 6, 7, 8, 9, 10]}),
   Produce.create({type: 'vegetable', name: 'Tomatoes', src: '../assets/tomatoes.jpg', available: [6, 7, 8, 9, 10, 11]}),
+  Produce.create({type: 'vegetable', name: 'Turnips', src: '../assets/turnips.jpg', available: [1, 2]}),
+  Produce.create({type: 'vegetable', name: 'Winter Squash', src: '../assets/wintersquash.jpg', available: [10, 11, 23]}),
+  Produce.create({type: 'vegetable', name: 'Zucchini', src: '../assets/zucchini.jpg', available: [4, 5, 6, 7, 8]}),
   Produce.create({type: 'fruit', name: 'Apples', src: '../assets/apples.jpg', available: [8, 9, 10, 11]}),
   Produce.create({type: 'fruit', name: 'Avacado', src: '../assets/avacado.jpg', available: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]}),
   Produce.create({type: 'fruit', name: 'Bananas', src: '../assets/bananas.jpg', available: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]}),
@@ -93,7 +117,19 @@ export default Ember.Service.extend({
       newList = this.filterByValueKey('herb', 'type', newList);
     }
   
-    return newList;
+    return newList.sort(function(a, b) {
+        var nameA = a.name.toUpperCase(); // ignore upper and lowercase
+        var nameB = b.name.toUpperCase(); // ignore upper and lowercase
+        if (nameA < nameB) {
+          return -1;
+        }
+        if (nameA > nameB) {
+          return 1;
+        }
+
+        // names must be equal
+        return 0;
+      });
   }
   
 });
